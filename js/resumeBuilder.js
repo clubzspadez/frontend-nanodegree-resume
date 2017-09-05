543c35aea8fde7ff2c0711cd4d6cdff8f53a457b
Jonathan Rodriguez <clubzspadez@gmail.com>
2017-09-05 18:51:53 -0400

    updates for js/index

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,14 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+var name = "Jonathan Rodriguez";
+var formattedName = HTMLheaderName.replace("%data%", name);
+var role = "Web Developer";
+var formattedRole = HTMLheaderRole.replace("%data%",role);
+
+
+$("#header").prepend(formattedName);
+$("#header").prepend(formattedRole);
+
+
